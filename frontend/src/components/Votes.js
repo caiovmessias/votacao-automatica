92711f036926aa78eee62755662aacc046bf2330
Caio Vinicius Messias <caiov.messias@icloud.com>
2020-08-18 00:19:22 -0300

    Finalização do projeto utilizado na aula-10 do bootcamp

---
+++ frontend/src/components/Votes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CountUp from 'react-countup';
+
+export default function Votes({ value, previous }) {
+  return (
+    <CountUp start={previous || 0} end={value} duration={0.6} separator=".">
+      {({ countUpRef }) => (
+        <div>
+          <span ref={countUpRef} />
+        </div>
+      )}
+    </CountUp>
+  );
+}
